e3d9b8f03e8b3cf13ce18e14e72f3e84abdc9e12
Vyacheslav Nikolaev <slavkapiyavka@yandex.ru>
2024-01-27 21:04:24 +0100

    feat: implement routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,21 +1,43 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import IndexView from '../views/IndexView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'index',
+      component: IndexView
     },
     {
       path: '/about',
       name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
       component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/projects',
+      name: 'projects',
+      component: () => import('../views/ProjectsView.vue')
+    },
+    {
+      path: '/engineering',
+      name: 'engineering',
+      component: () => import('../views/EngineeringView.vue')
+    },
+    {
+      path: '/cooperation',
+      name: 'cooperation',
+      component: () => import('../views/CooperationView.vue')
+    },
+    {
+      path: '/blog',
+      name: 'blog',
+      component: () => import('../views/BlogView.vue')
+    },
+    {
+      path: '/contacts',
+      name: 'contacts',
+      component: () => import('../views/ContactsView.vue')
     }
   ]
 })
